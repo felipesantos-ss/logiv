9e4ac93a321d0d95f2e19af3ed13041d56004298
Felipe <felipesantoss427@gmail.com>
2025-09-16 17:41:39 -0300

    freight search by tracking code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
                             <h2 class="text-xl font-semibold text-gray-900">
                                 Rastrear encomenda
                             </h2>
-                            <form action="#" method="GET" class="flex items-center space-x-2">
+                            <form action="{{ route('frete.rastreamento') }}" method="GET" class="flex items-center space-x-2">
                                 <div class="relative w-full max-w-md">
                                     <input type="text" name="codigo_rastreio" placeholder="Código de rastreamento" class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-600 focus:border-green-600">
                                     <button type="submit" class="absolute inset-y-0 right-0 px-4 py-2 text-white bg-green-600 rounded-r-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600">Consultar</button>
